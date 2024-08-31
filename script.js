8e398e921427b07e28071fdb6c99db29888ce46f
Sahil Sharma <141577217+SahilWMI@users.noreply.github.com>
2024-09-01 00:27:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function make2DArray(cols, rows) {
-    let arr = new Array(cols);
+    let arr = new Array(Math.floor(cols));
     for (let i = 0; i < arr.length; i++) {
-        arr[i] = new Array(rows);
+        arr[i] = new Array(Math.floor(rows));
+        for (let j = 0; j < arr[i].length; j++){
+            arr[i][j] = 0;
+        }
     }
     return arr;
 }
@@ -12,15 +15,11 @@ let cols, rows;
 
 function setup() {
     createCanvas(400, 400);
-    cols = width / w;
-    rows = height / w;
+    cols = Math.floor(width / w);
+    rows = Math.floor(height / w);
     grid = make2DArray(cols, rows);
 
-    for(let i = 0; i < cols; i++){
-        for(let j = 0; j < rows; j++){
-            grid[i][j] = 0;
-        }
-    }
+    grid[20][10] = 1;
 }
 
 function draw() {
@@ -29,10 +28,35 @@ function draw() {
     for(let i = 0; i < cols; i++){
         for(let j = 0; j < rows; j++){
             stroke(255);
-            fill(grid[i][j]*255);
+            fill(grid[i][j] * 255);
             let x = i * w;
             let y = j * w;
             square(x, y, w);
         }
     }
+
+    let nextGrid = make2DArray(cols, rows);
+    
+    for(let i = 0; i < cols; i++) {
+        for(let j = 0; j < rows; j++) {
+            let state = grid[i][j];
+
+            if (state === 1) {
+                if (j + 1 < rows && grid[i][j + 1] === 0) {
+                    nextGrid[i][j + 1] = 1;
+                } 
+                else if (i - 1 >= 0 && j + 1 < rows && grid[i - 1][j + 1] === 0) {
+                    nextGrid[i - 1][j + 1] = 1;
+                } 
+                else if (i + 1 < cols && j + 1 < rows && grid[i + 1][j + 1] === 0) {
+                    nextGrid[i + 1][j + 1] = 1;
+                } 
+                else {
+                    nextGrid[i][j] = 1;
+                }
+            }
+        }  
+    }
+
+    grid = nextGrid;
 }
